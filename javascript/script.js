92ca2dc3cda1a68870fa5a56d9fcc319ae917aad
millerm30 <millergm30@gmail.com>
2022-12-30 15:03:26 -0330

    images setup...

--- javascript/script.js
+++ javascript/script.js
@@ -6,16 +6,16 @@ const randomJokeUrl ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type
 const imageUrls = [
   "images/2.png",
   "images/3.png",
-  "jokes/images/4.png",
-  "jokes/images/5.png",
-  "jokes/images/6.png",
-  "jokes/images/7.png",
-  "jokes/images/8.png",
-  "jokes/images/9.png",
-  "jokes/images/10.png",
-  "jokes/images/11.png",
-  "jokes/images/12.png",
-  "jokes/images/13.png",
+  "images/4.png",
+  "images/5.png",
+  "images/6.png",
+  "images/7.png",
+  "images/8.png",
+  "images/9.png",
+  "images/10.png",
+  "images/11.png",
+  "images/12.png",
+  "images/13.png",
 ];
 
 generateBtn.onclick = regenerate;
